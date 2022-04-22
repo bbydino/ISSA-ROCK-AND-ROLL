9f45dd321fc1919acc7269f18e7e58022985d496
bbydino <daniel.ngo16@gmail.com>
2022-04-22 02:51:53 -0500

    add myNRML to maxlik functions

--- R/mymaxlik.r
+++ R/mymaxlik.r
@@ -69,3 +69,85 @@ mymaxlik = function(lfun, x, param, ...) {
     slope = slope
   ))
 }
+
+
+#' Maximum Likelihood using Newton-Raphson Algorithm
+#'
+#' Maximum Likelihood using Newton-Raphson Algorithm on the derivative
+#'
+#' From: MATH 4753 Lab 10
+#'
+#' @importFrom grDevices rainbow
+#' @importFrom graphics barplot
+#'
+#' @param x0 initial x-value for algorithm
+#' @param delta delta for algorithm, default 0.001
+#' @param llik likelihood function
+#' @param xrange x range for algorithm
+#' @param parameter parameter of interest to the lfun function
+#'
+#' @return a list containing:
+#' x values from algorithm
+#' y values from algorithm
+#'
+#' @importFrom graphics axis points
+#'
+#' @example
+#' lfun = function(x) log(dpois(4,x)*dpois(6,x)*dpois(7,x)*dpois(6,x)*dpois(5,x))
+#' myNRML(x0=1,delta=0.000001,llik=lfun,xrange=c(0,20),parameter="lambda")
+#' @export
+myNRML = function(x0,
+                  delta = 0.001,
+                  llik,
+                  xrange,
+                  parameter = "param") {
+  f = function(x)
+    (llik(x + delta) - llik(x)) / delta
+  fdash = function(x)
+    (f(x + delta) - f(x)) / delta
+  d = 1000
+  i = 0
+  x = c()
+  y = c()
+  x[1] = x0
+  y[1] = f(x[1])
+  while (d > delta & i < 100) {
+    i = i + 1
+    x[i + 1] = x[i] - f(x[i]) / fdash(x[i])
+    y[i + 1] = f(x[i + 1])
+    d = abs(y[i + 1])
+  }
+  layout(matrix(
+    1:2,
+    nrow = 1,
+    ncol = 2,
+    byrow = TRUE
+  ), widths = c(1, 2))
+  curve(
+    llik(x),
+    xlim = xrange,
+    xlab = parameter,
+    ylab = "log Lik",
+    main = "Log Lik"
+  )
+  curve(
+    f(x),
+    xlim = xrange,
+    xaxt = "n",
+    xlab = parameter,
+    ylab = "derivative",
+    main =  "Newton-Raphson Algorithm \n on the derivative"
+  )
+  points(x,
+         y,
+         col = "Red",
+         pch = 19,
+         cex = 1.5)
+  axis(1, x, round(x, 2), las = 2)
+  abline(h = 0, col = "Red")
+
+  segments(x[1:(i - 1)], y[1:(i - 1)], x[2:i], rep(0, i - 1), col = "Blue", lwd =2)
+  segments(x[2:i], rep(0, i - 1), x[2:i], y[2:i], lwd = 0.5, col = "Green")
+
+  return(list(x = x, y = y))
+}
